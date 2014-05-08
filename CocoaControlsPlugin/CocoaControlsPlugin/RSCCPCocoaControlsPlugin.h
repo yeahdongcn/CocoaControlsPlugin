8e7f772bd1212211034202a2f173675a14b933d2
R0CKSTAR <yeahdongcn@gmail.com>
2014-05-08 17:29:17 +0800

    Add empty Xcode plugin project

---
+++ CocoaControlsPlugin/CocoaControlsPlugin/RSCCPCocoaControlsPlugin.h
@@ -0,0 +1,13 @@
+//
+//  RSCCPCocoaControlsPlugin.h
+//  RSCCPCocoaControlsPlugin
+//
+//  Created by R0CKSTAR on 5/8/14.
+//  Copyright (c) 2014 P.D.Q. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface RSCCPCocoaControlsPlugin : NSObject
+
+@end
